f5a9f925a4d1b53fd6ecd9d5dcb4ebadd0916570
Vinutna-ulisi <vinutnaulisi727@gmail.com>
2026-06-09 16:40:31 +0530

    Add files via upload

---
+++ leetcode/prac5.java
@@ -0,0 +1,13 @@
+CONVERT BINARY NUM IN LL TO INTEGER
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int result = 0;
+
+        while (head != null) {
+            result = result * 2 + head.val;
+            head = head.next;
+        }
+
+        return result;
+    }
+}
